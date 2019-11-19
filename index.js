f652887ea288aca19f577dd8be057b1c28cd05ef
Lamby777 <sheepareawesome777@gmail.com>
2019-11-19 16:11:46 +0000

    changed comment tag at top of screen

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-/*
--------------------------------------
-|            ~Slurp~                |
-|         By DexieTheSheep          |
-| https://github.com/Lamby777/Slurp |
--------------------------------------
-*/
+/*	-------------------------------------
+	|            ~Slurp~                |
+	|         By DexieTheSheep          |
+	| https://github.com/Lamby777/Slurp |
+	------------------------------------- */
 
 const files = require("fs"); // file reading
 const fn = require("./func"); // grammar, io, and other
 
 let read = files.createReadStream("code.dx", "utf8");
 read.on("data", (ch) => {
-	fn.out(fn.lex(ch));
+	fn.lex(ch)
 });
 read.on("end", () => {
 	console.log("<|EOF|>");
